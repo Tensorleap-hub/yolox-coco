8b3fe8b72009448a2f0973068d6c8f34fd45592a
ranhomri <ran.homri@tensorleap.ai>
2023-12-17 12:24:19 +0200

    data mock

--- data/mock_metropolis_structure.py
+++ data/mock_metropolis_structure.py
@@ -4,15 +4,15 @@ from json import load, dump
 from yolox.config import annotation_files_dir, CONFIG
 
 train_ann_path = os.path.join(annotation_files_dir, CONFIG['train_file'])
-val_ann_path = os.path.join(annotation_files_dir, CONFIG['val_file'])
 
-for path in [train_ann_path, val_ann_path]:
+for path in [train_ann_path]:
     with open(path, 'r') as f:
         ann_file = load(f)
 
     # Edit all file names
-    for image in ann_file["images"]:
-        image["file_name"] = f"../../images/{image['file_name']}"
+    for i, image in enumerate(ann_file["images"]):
+        if i % 50 == 0:
+            image["file_name"] = f"../../true_negative_images/{image['file_name']}"
 
     with open(path, 'w') as f:
         dump(ann_file, f)
